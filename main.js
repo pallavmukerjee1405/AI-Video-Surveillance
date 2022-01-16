2164c58bfc4f11ffcb730d6376f8555107661306
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2022-01-16 11:21:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+video="";
+status_quo="";
+
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+
 function setup(){
     canvas=createCanvas(480,380);
     canvas.center();
-    video.hide();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+    status_quo=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
 }
