8b4b9d22ef12a50be3411b877b8bd1adfd6bc0b1
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2022-01-23 11:44:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status_quo="";
+objects="";
 
 function preload(){
     video=createVideo('video.mp4');
@@ -13,6 +14,21 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(status_quo !=""){
+        objectDetector.detect(video,gotResult);
+
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML="Number of Objects Detected : "+objects.length;
+
+            fill("FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
 
 function start(){
@@ -26,4 +42,15 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error,results){
+     if(error){
+         console.error(error);
+     }
+     {
+         console.log(results);
+         objects=results;
+         
+     }
 }
